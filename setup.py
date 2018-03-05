d2266428be37bd56e48fc0ca4f092ec9ad93e44a
Julien Legras <julien.legras@synacktiv.com>
2018-03-05 17:37:11 +0100

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "ajpy",
     packages = ["ajpy"],
-    version = "0.0.1",
+    version = "0.0.3",
     description = "AJP package crafting library",
     author = "Julien Legras",
     author_email = "julien.legras@synacktiv.com",

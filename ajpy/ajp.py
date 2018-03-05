978a5b5476ed80a845e76dab3e7a14c940a90f01
Julien Legras <julien.legras@synacktiv.com>
2018-03-05 17:29:22 +0100

    Clarify license to BSD-4 clause

--- ajpy/ajp.py
+++ ajpy/ajp.py
@@ -1,4 +1,19 @@
-# Julien Legras - Synacktiv
+# Copyright (c) 2018, Julien Legras <julien.legras@synacktiv.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#    This product includes software developed by the <organization>.
+# 4. Neither the name of the <organization> nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY SYNACKTIV ''AS IS'' AND ANY
 # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED

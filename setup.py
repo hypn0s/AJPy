33be8b43a002d114729b1786c8d88ed5f35c0499
Julien Legras <julien.legras@synacktiv.com>
2018-03-12 09:55:29 +0100

    Bumped version in setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "ajpy",
     packages = ["ajpy"],
-    version = "0.0.3",
+    version = "0.0.4",
     description = "AJP package crafting library",
     author = "Julien Legras",
     author_email = "julien.legras@synacktiv.com",
